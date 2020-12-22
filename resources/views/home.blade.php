f7965bcde3dfc56179ddeb79eb61cc51258bc97a
icatpojan <irsyadfauzangg@gmail.com>
2020-12-22 11:36:08 +0700

    menambahkan dshboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,97 @@
-@extends('layouts.app')
+<!-- FUNGSI EXTENDS DIGUNAKAN UNTUK ME-LOAD MASTER LAYOUTS YANG ADA, KARENA INI ADALAH HALAMAN HOME MAKA KITA ME-LOAD LAYOUTS ADMIN.BLADE.PHP -->
+<!-- KETIKA MELOAD FILE BLADE, MAKA EKSTENSI .BLADE.PHP TIDAK PERLU DITULISKAN -->
+@extends('layouts.admin')
 
+<!-- TAG YANG DIAPIT OLEH SECTION('TITLE') AKAN ME-REPLACE @YIELD('TITLE') PADA MASTER LAYOUTS -->
+@section('title')
+    <title>Dashboard</title>
+@endsection
+
+<!-- TAG YANG DIAPIT OLEH SECTION('CONTENT') AKAN ME-REPLACE @YIELD('CONTENT') PADA MASTER LAYOUTS -->
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+    <main class="main">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item">Home</li>
+            <li class="breadcrumb-item active">Dashboard</li>
+        </ol>
+        <div class="container-fluid">
+            <div class="animated fadeIn">
+                <div class="row">
+                    <div class="col-md-12">
+                       {{-- <h1>SELAMAT DATANG PAK {{ ($Customer) }}</h1> --}}
+                       <p>semuanya berjalan normal walau hati sedang kacau dan kesepian</p>
+                        <div class="card">
+                            <div class="card-header">
+                                <h4 class="card-title">perkembangan</h4>
+                            </div>
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-md-3">
+                                        <div class="callout callout-info">
+                                            <small class="text-muted">jumlah uang masuk</small>
+                                            <br>
+                                            {{-- <strong class="h4">{{ number_format($Payment) }}</strong> --}}
+                                        </div>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <div class="callout callout-danger">
+                                            <small class="text-muted">Jumlah member</small>
+                                            <br>
+                                            {{-- <strong class="h4">{{ number_format($User) }}</strong> --}}
+                                        </div>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <div class="callout callout-primary">
+                                            <small class="text-muted">Perlu Dikirim</small>
+                                            <br>
+                                            {{-- <strong class="h4">{{ number_format($Order) }}</strong> --}}
+                                        </div>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <div class="callout callout-success">
+                                            <small class="text-muted">Total Produk</small>
+                                            <br>
+                                            {{-- <strong class="h4">{{ number_format($Product) }}</strong> --}}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                        <div class="card-deck">
+                            <div class="card mb-5">
+                                <div class="card-body">
+                                    <h5 class="card-title">rata rata jumlah transaksi</h5>
+                                    <div class="callout callout-success">
+                                        <small class="text-muted">dalam rupiah</small>
+                                        <br>
+                                        {{-- <strong class="h4">{{ number_format($Transaksi) }}</strong> --}}
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card mb-5">
+                                <div class="card-body">
+                                    <h5 class="card-title">Toko terdaftar</h5>
+                                    <div class="callout callout-success">
+                                        <small class="text-muted">jumlah toko</small>
+                                        <br>
+                                        {{-- <strong class="h4">{{ ($Shop) }}</strong> --}}
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card mb-5">
+                                <div class="card-body">
+                                    <h5 class="card-title">produk habis</h5>
+                                    <div class="callout callout-success">
+                                        <small class="text-muted">Total Produk</small>
+                                        <br>
+                                        {{-- <strong class="h4">{{ number_format($Produk) }}</strong> --}}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</div>
+    </main>
 @endsection
---
+++ resources/views/layouts/module/sidebar.blade.php
@@ -0,0 +1,43 @@
+<nav class="sidebar-nav">
+    <ul class="nav">
+        <li class="nav-item">
+            <a class="nav-link" href="">
+                <i class="nav-icon icon-speedometer"></i> Dashboard
+            </a>
+        </li>
+
+        <li class="nav-title">MANAJEMEN PRODUK</li>
+        <li class="nav-item">
+            <a class="nav-link" href="">
+                <i class="nav-icon icon-target"></i> Kategori sampah
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="">
+                <i class="nav-icon icon-cursor"></i> transaksi
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="">
+                <i class="nav-icon icon-user"></i> Nasabah
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="">
+                <i class="nav-icon icon-basket"></i> 
+            </a>
+        </li>
+        <li class="nav-item nav-dropdown">
+            <a class="nav-link nav-dropdown-toggle" href="#">
+                <i class="nav-icon icon-settings"></i> Pengaturan
+            </a>
+            <ul class="nav-dropdown-items">
+                <li class="nav-item">
+                    <a class="nav-link" href="">
+                        <i class="nav-icon icon-puzzle"></i>User black list
+                    </a>
+                </li>
+            </ul>
+        </li>
+    </ul>
+</nav>
