333560dc04511eab9013bca98c14c46a5fee0a12
icatpojan <irsyadfauzangg@gmail.com>
2020-12-22 14:30:25 +0700

    user

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Auth;
+use App\User;
+use Illuminate\Http\Request;
+use Hash;
+use GuzzleHttp\Client;
+
+class ProfileController extends Controller
+{
+    public function update(Request $request)
+    {
+        $this->validate($request, [
+            'password'  => 'requiredconfirmed',
+            'phone_number'  => 'string',
+            
+        ]);
+        $image = null;
+
+        if ($request->image) {
+            $img = base64_encode(file_get_contents($request->image));
+            $client = new Client();
+            $res = $client->request('POST', 'https://freeimage.host/api/1/upload', [
+                'form_params' => [
+                    'key' => '6d207e02198a847aa98d0a2a901485a5',
+                    'action' => 'upload',
+                    'source' => $img,
+                    'format' => 'json',
+                ]
+            ]);
+            $array = json_decode($res->getBody()->getContents());
+            $image = $array->image->file->resource->chain->image;
+        }
+
+        $user = User::where('id', Auth::user()->id)->first();
+        $user->avatar = $image;
+        $user->phone_number = $request->phone_number;
+        $user->address = $request->address;
+
+        if (!empty($request->password)) {
+            $user->password = Hash::make($request->password);
+        }
+
+        $user->update();
+        return $this->sendResponse('Success', 'profile berhasil di update Bos', $user, 500);
+    }
+    public function change(Request $request)
+    {
+        $this->validate($request, [
+            'password'  => 'requiredconfirmed',
+        ]);
+        $user = User::where('id', Auth::user()->id)->first();
+        $user->password = $request->password_change;
+        $user->update();
+        if (!empty($request->password_change)) {
+            $user->password = Hash::make($request->password_change);
+        }
+
+    }
+}
