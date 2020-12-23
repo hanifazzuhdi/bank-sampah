24258b5360e1d821dfb6e148bf5b1e8629c9d66d
icatpojan <irsyadfauzangg@gmail.com>
2020-12-23 09:39:12 +0700

    penarikan admin

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
 Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['namespace' => 'Web', 'middleware' => 'Auth'], function () {
+    Route::post('tarik', 'PenarikanController@tarik');
+});
+
