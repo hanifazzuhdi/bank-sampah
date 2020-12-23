47e581bf221fb53d9b5a45cb36774450eb6c51a1
icatpojan <irsyadfauzangg@gmail.com>
2020-12-23 17:30:31 +0700

    penjemputan

--- app/Model/Chat.php
+++ app/Model/Chat.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Chat extends Model
 {
-    //
+    protected $fillable = ['from', 'to', 'message', 'is_read'];
+
+    public function getCreatedAtAttribute()
+    {
+        return \Carbon\Carbon::parse($this->attributes['created_at'])
+            ->format('H:i');
+    }
 }
