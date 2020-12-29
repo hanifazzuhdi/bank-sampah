06c87e7b10f73817be63e1ecb25b2545aeaa6fc2
icatpojan <irsyadfauzangg@gmail.com>
2020-12-29 15:06:04 +0700

    dashboard

--- routes/web.php
+++ routes/web.php
@@ -35,4 +35,6 @@ Route::group(['namespace' => 'Web', 'middleware' => ['auth', 'user.web']], funct
     Route::get('trash', 'UserController@trash')->name('trash');//menampilkan data user terblack list
     Route::get('trash/{id}', 'UserController@restore')->name('restore');//mengembalikan data user
     Route::get('permanen/{id}', 'UserController@hapus_permanen')->name('permanen');//hapus permanen user
+    Route::get('gudang', 'SampahController@index')->name('gudang');//menampilkan data user terblack list
+
 });
