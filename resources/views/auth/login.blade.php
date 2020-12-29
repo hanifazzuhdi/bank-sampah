ba6aa042507ef9ac49ab261fbe677edad7f321c4
hanifazzuhdi <hnfhanif52@gmail.com>
2020-12-29 21:39:08 +0700

    update dashboard auth

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,63 +1,60 @@
 @extends('layouts.auth')
 
-@section('title')
-<title>Login</title>
-@endsection
-
 @section('content')
-<div class="row justify-content-center">
-    <div class="col-md-8">
-        <div class="card-group">
-            <div class="card p-4">
-                <div class="card-body">
-                    <h1>Login</h1>
-                    <p class="text-muted">Sign In to your account</p>
-                    <form action="{{ route('login') }}" method="post">
-                        @csrf
-                        <div class="input-group mb-3">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text">
-                                    <i class="icon-user"></i>
-                                </span>
-                            </div>
-                            <input class="form-control {{ $errors->has('email') ? ' is-invalid' : '' }}" type="text"
-                                name="email" placeholder="Email Address" value="{{ old('email') }}" autofocus required>
-                        </div>
-                        <div class="input-group mb-4">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text">
-                                    <i class="icon-lock"></i>
-                                </span>
-                            </div>
-                            <input class="form-control {{ $errors->has('email') ? ' is-invalid' : '' }}" type="password"
-                                name="password" placeholder="Password" required>
-                        </div>
-                        <div class="row">
-                            @if (session('error'))
-                            <div class="col-md-12">
-                                <div class="alert alert-danger" role="alert">
-                                    {{ session('error') }}
+<div class="container mt-5">
+
+    <!-- Outer Row -->
+    <div class="row justify-content-center">
+
+        <div class="col-xl-10 col-lg-12 col-md-9">
+
+            <div class="card o-hidden border-0 shadow-lg my-5">
+                <div class="card-body p-0">
+                    <!-- Nested Row within Card Body -->
+                    <div class="row">
+                        <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                        <div class="col-lg-6">
+                            <div class="p-5">
+                                <div class="text-center">
+                                    <h1 class="h4 text-gray-900 mb-5">WELCOME BACK !</h1>
+                                </div>
+
+                                <form class="user mb-5" method="POST" action="{{route('login')}}">
+                                    <div class="form-group">
+                                        <input type="email" name="email" class="form-control form-control-user"
+                                            id="exampleInputEmail" aria-describedby="emailHelp"
+                                            placeholder="Enter Email Address...">
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="password" name="password" class="form-control form-control-user"
+                                            id="exampleInputPassword" placeholder="Password">
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="custom-control custom-checkbox small">
+                                            <input type="checkbox" class="custom-control-input" id="customCheck">
+                                            <label class="custom-control-label" for="customCheck">Remember
+                                                Me</label>
+                                        </div>
+                                    </div>
+                                    <button class="btn btn-primary btn-user btn-block">
+                                        Login
+                                    </button>
+                                    <hr>
+                                    @csrf
+                                </form>
+
+                                <div class="text-center">
+                                    <a class="small" href="{{route('password.request')}}">Forgot Password?</a>
                                 </div>
                             </div>
-                            @endif
-
-                            <div class="col-6">
-                                <button class="btn btn-primary px-4">Login</button>
-                            </div>
-                            <div class="col-6 text-right">
-                                <a href="{{route('password.request')}}" role="button" , class="btn btn-link px-0">Forgot
-                                    Password</a>
-                            </div>
                         </div>
-                    </form>
+                    </div>
                 </div>
             </div>
-            <div class="card text-white bg-primary py-5 d-md-down-none" style="width:44%">
-                <div class="card-body text-center">
 
-                </div>
-            </div>
         </div>
+
     </div>
+
 </div>
 @endsection
--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -2,43 +2,40 @@
 <html lang="en">
 
 <head>
+
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <meta name="description" content="CoreUI - Open Source Bootstrap Admin Template">
-    <meta name="author" content="Łukasz Holeczek">
-    <meta name="keyword" content="Bootstrap,Admin,Template,Open,Source,jQuery,CSS,HTML,RWD,Dashboard">
-    
-    @yield('title')
-    
-    <link href="{{ asset('assets/css/coreui-icons.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/css/flag-icon.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/css/font-awesome.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/css/simple-line-icons.css') }}" rel="stylesheet">
-    
-    <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/css/pace.min.css') }}" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Sampah - Login Dashboard</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="{{asset('template/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="{{asset('template/css/sb-admin-2.min.css')}}" rel="stylesheet">
+
 </head>
 
-<body class="app flex-row align-items-center">
-    <div class="container">
-        @yield('content')
-    </div>
+<body class="bg-gradient-primary">
 
-    <script src="{{ asset('assets/js/jquery.min.js') }}"></script>
-    <script src="{{ asset('assets/js/popper.min.js') }}"></script>
-    <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
-    <script src="{{ asset('assets/js/pace.min.js') }}"></script>
-    <script src="{{ asset('assets/js/perfect-scrollbar.min.js') }}"></script>
-    <script src="{{ asset('assets/js/coreui.min.js') }}"></script>
-    <script>
-        $('#ui-view').ajaxLoad();
-        $(document).ajaxComplete(function() {
-            Pace.restart()
-        });
+    @yield('content');
 
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+    <!-- Bootstrap core JavaScript-->
+    <script src="{{asset('template/vendor/jquery/jquery.min.js')}}"></script>
+    <script src="{{asset('template/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="{{asset('template/vendor/jquery-easing/jquery.easing.min.js')}}"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="{{asset('template/js/sb-admin-2.min.js')}}"></script>
+
 </body>
 
 </html>
