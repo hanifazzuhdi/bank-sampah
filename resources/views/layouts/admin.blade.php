d144835f288d61e54d764ad2defd1823f83d632b
hanifazzuhdi <hnfhanif52@gmail.com>
2020-12-29 21:55:18 +0700

    update home view

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -2,60 +2,47 @@
 <html lang="en">
 
 <head>
+
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <meta name="description" content="Daengweb - Aplikasi Ecommerce">
-    <meta name="author" content="Daengweb">
-    <meta name="keyword"
-        content="aplikasi ecommerce laravel, tutorial laravel basic, belajar laravel, panduan belajar laravel">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-    @yield('title')
+    <title>Sammpah - Dashboard Admin</title>
 
-    <!-- UNTUK ME-LOAD ASSET DARI PUBLIC, KITA GUNAKAN HELPER ASSET() -->
-    <link href="{{ asset('assets/css/font-awesome.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/css/simple-line-icons.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/vendors/pace-progress/css/pace.min.css') }}" rel="stylesheet">
+    <!-- Custom fonts for this template-->
+    <link href="{{asset('template/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="{{asset('template/css/sb-admin-2.min.css')}}" rel="stylesheet">
+
 </head>
 
-<body class="app header-fixed sidebar-fixed aside-menu-fixed sidebar-lg-show">
+<body id="page-top">
 
-    @include('layouts.module.header')
+    @yield('content')
 
-    <div class="app-body" id="dw">
-        <div class="sidebar">
+    <!-- Bootstrap core JavaScript-->
+    <script src="{{asset('template/vendor/jquery/jquery.min.js')}}"></script>
+    <script src="{{asset('template/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 
-            <!-- SIDEBAR JUGA KITA PISAHKAN CODENYA MENJADI FILE TERSENDIRI -->
-            <!-- KETIKA MELOAD FILE BLADE, MAKA EKSTENSI .BLADE.PHP TIDAK PERLU DITULISKAN -->
-            @include('layouts.module.sidebar')
+    <!-- Core plugin JavaScript-->
+    <script src="{{asset('template/vendor/jquery-easing/jquery.easing.min.js')}}"></script>
 
-        </div>
+    <!-- Custom scripts for all pages-->
+    <script src="{{asset('template/js/sb-admin-2.min.js')}}"></script>
 
-        <!-- BAGIAN INI AKAN DI-REPLACE SESUAI ISI YANG DIAPIT DARI @SECTION('CONTENT') -->
-        @yield('content')
+    <!-- Page level plugins -->
+    <script src="{{asset('template/vendor/chart.js/Chart.min.js')}}"></script>
 
-    </div>
+    <!-- Page level custom scripts -->
+    <script src="{{asset('template/js/demo/chart-area-demo.js')}}"></script>
+    <script src="{{asset('template/js/demo/chart-pie-demo.js')}}"></script>
 
-    <footer class="app-footer">
-        <div>
-            <a href="https://si--amanah.herokuapp.com">SAMPAH</a>
-            <span>&copy; <?= date('Y') ?></span>
-        </div>
-        <div class="ml-auto">
-            <span>Powered by</span>
-            <a href="https://si--amanah.herokuapp.com">RHF</a>
-        </div>
-    </footer>
-
-    <script src="{{ asset('assets/js/jquery.min.js') }}"></script>
-    <script src="{{ asset('assets/js/popper.min.js') }}"></script>
-    <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
-    <script src="{{ asset('assets/js/pace.min.js') }}"></script>
-    <script src="{{ asset('assets/js/perfect-scrollbar.min.js') }}"></script>
-    <script src="{{ asset('assets/js/coreui.min.js') }}"></script>
-    <script src="{{ asset('assets/js/custom-tooltips.min.js') }}"></script>
-    @yield('js')
 </body>
 
 </html>
