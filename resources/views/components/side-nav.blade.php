1359a52bd3331bbc31e8aa2d23cd217d1c4362ad
hanifazzuhdi <hnfhanif52@gmail.com>
2020-12-30 08:51:30 +0700

    update dashboard

--- resources/views/components/side-nav.blade.php
+++ resources/views/components/side-nav.blade.php
@@ -48,13 +48,13 @@
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
             aria-expanded="true" aria-controls="collapseUtilities">
             <i class="fas fa-fw fa-users"></i>
-            <span>Kelola Karyawan</span>
+            <span>Kelola User</span>
         </a>
         <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Kelola Karyawan</h6>
+                <h6 class="collapse-header">Kelola : </h6>
                 <a class="collapse-item" href="utilities-color.html">Daftar Karyawan</a>
-                <a class="collapse-item" href="utilities-border.html">Gaji Karyawan</a>
+                <a class="collapse-item" href="utilities-border.html">Daftar Pengguna</a>
                 <a class="collapse-item" href="utilities-animation.html"></a>
             </div>
         </div>
