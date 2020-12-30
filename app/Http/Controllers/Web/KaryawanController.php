bbe7b49f5045f98490266640b9a2c4ba2a226f57
hanifazzuhdi <hnfhanif52@gmail.com>
2020-12-30 16:02:51 +0700

    update dashboard karyawan

--- app/Http/Controllers/Web/KaryawanController.php
+++ app/Http/Controllers/Web/KaryawanController.php
@@ -13,4 +13,28 @@ class KaryawanController extends Controller
 
         return view('pages.karyawan', compact('users'));
     }
+
+    // for jquery
+    public function show($id)
+    {
+        $data = User::find($id);
+
+        switch (true) {
+            case $data->role_id == 4:
+                $data->role_id = 'Bendahara';
+                break;
+            case $data->role_id == 2:
+                $data->role_id = 'Pengurus 1';
+                break;
+            case $data->role_id == 3:
+                $data->role_id = 'Pengurus 2';
+                break;
+        }
+
+        echo json_encode($data);
+    }
+
+    public function update()
+    {
+    }
 }
--- resources/views/pages/karyawan.blade.php
+++ resources/views/pages/karyawan.blade.php
@@ -2,6 +2,21 @@
 
 @section('style')
 <link href="{{asset('template/vendor/datatables/dataTables.bootstrap4.min.css')}}" rel="stylesheet">
+
+<style>
+    .updateAvatar {
+        position: absolute;
+        bottom: 40%;
+        right: 52%;
+        transform: translateX(50%);
+    }
+
+    .updateAvatar input {
+        width: 90px;
+        color: rgba(255, 255, 255, 0);
+    }
+</style>
+
 @endsection
 
 @section('content')
@@ -13,8 +28,11 @@
 
     <!-- DataTales Example -->
     <div class="card shadow mb-4">
-        <div class="card-header py-3">
+        <div class="card-header d-flex justify-content-between py-3">
             <h6 class="m-0 font-weight-bold text-primary">Daftar karyawan</h6>
+            <button class="btn btn-primary btn-sm" data-toggle="modal" data-target=".modal-karyawan">
+                <i class="fas fa-plus"></i>
+            </button>
         </div>
         <div class="card-body">
             <div class="table-responsive">
@@ -49,7 +67,7 @@
                             <td>{{$user->created_at}}</td>
                             <td class="text-center">
                                 <a class="see" href="#" data-id="{{$user->id}}" data-toggle="modal"
-                                    data-target=".modal-karyawan" data-url="{{env('APP_URL') . '/karyawan/edit'}}">
+                                    data-target=".modal-karyawan" data-url="{{env('APP_URL') . '/karyawan/'}}">
                                     <i class="fas fa-eye"></i>
                                 </a>
                             </td>
@@ -61,125 +79,19 @@
         </div>
     </div>
 
-    {{-- Modal --}}
-    <div class="modal fade modal-karyawan" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel"
-        aria-hidden="true">
-        <div class="modal-dialog modal-lg">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h6 class="text-primary font-weight-bold pt-2">DETAIL USER</h6>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <div class="row">
-                        <div class="col-md-4 d-flex justify-content-center border-right pt-4">
-                            <img class="rounded-circle" src="https://via.placeholder.com/150" alt="Avatar" width="150px"
-                                height="150px">
-                        </div>
-                        <div class="col-md-8 p-3">
-                            <form action="" method="post">
-                                <div class="row mb-2">
-                                    <div class="col">
-                                        <label>Nama : </label>
-                                        <input type="text" class="form-control" name="nama">
-                                    </div>
-                                    <div class="col">
-                                        <label>Email : </label>
-                                        <input type="email" class="form-control" disabled>
-                                    </div>
-                                </div>
-
-                                <div class="row mb-2">
-                                    <div class="col">
-                                        <label>No. Telepon : </label>
-                                        <input type="text" class="form-control" name="phone_number">
-                                    </div>
-
-                                    <div class="col">
-                                        <label>Role : </label>
-                                        <input type="text" class="form-control" disabled>
-                                    </div>
-                                </div>
-
-                                <div class="form-group mb-2">
-                                    <label>Alamat</label>
-                                    <textarea class="form-control" name="address" cols="30" rows="3"></textarea>
-                                </div>
-
-                                <div class="row">
-                                    <div class="col">
-                                        <label>Created At : </label>
-                                        <input class="form-control" type="text" disabled>
-                                    </div>
-                                    <div class="col">
-                                        <label>Updated At : </label>
-                                        <input class="form-control" type="text" disabled>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal" aria-label="Close">
-                        Close
-                    </button>
-                    <button class="btn btn-warning" onclick="return confirm('Yakin Ubah ?')">
-                        Update
-                    </button>
-                </div>
-            </div>
-        </div>
-    </div>
+    @include('components.modal')
 
 </div>
 <!-- /.container-fluid -->
 
 @endsection
 
-
 @section('script')
 <script src="{{asset('template/vendor/datatables/jquery.dataTables.min.js')}}"></script>
 <script src="{{asset('template/vendor/datatables/dataTables.bootstrap4.min.js')}}"></script>
 <script src="{{asset('template/js/demo/datatables-demo.js')}}"></script>
 
-<script>
-    // Permintaan csrf token laravel
-       $.ajaxSetup({
-        headers: { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') }
-    });
-
-    // jquery + ajax update
-    $(function () {
-        $('.see').on('click', function(){
-
-            const id = $(this).data('id');
-            console.log(id);
-
-            const url = $(this).data('url');
-            console.log(url);
-
-            $('.modal-body form').attr('action', 'http://localhost:8000/' + id )
-
-            $.ajax({
-                url: 'http://localhost:8000/' + id ,
-                // data: {id:id},
-                method: 'get',
-                dataType: 'json',
-                success: function (data) {
-                    console.log(data)
-                    $('#productName').val(data.product_name);
-                    $('#sku').val(data.sku);
-                    $('#sell_price').val(data.sell_price);
-                    $('#buy_price').val(data.buy_price);
-                    $('#stok').val(data.quantity);
-                    $('#id').val(data.id);
-                }
-            });
-        });
-    });
-</script>
+{{-- jquery --}}
+<script src="{{asset('js/jquery.js')}}"></script>
 
 @endsection
