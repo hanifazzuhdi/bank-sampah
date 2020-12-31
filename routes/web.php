60eb3bc8b40009a47c7d5fb5e009e83cba1793ac
icatpojan <irsyadfauzangg@gmail.com>
2020-12-31 13:22:09 +0700

    pull

--- routes/web.php
+++ routes/web.php
@@ -46,12 +46,12 @@ Route::group(['namespace' => 'Web', 'middleware' => ['auth', 'user.web']], funct
     Route::get('sampah', 'SampahController@getSampah')->name('sampah.index'); //menampilkan data List sampah
     Route::get('gudang', 'SampahController@getGudang')->name('gudang.index'); //menampilkan data gudang sampah
 
-    //Route keuangan bank
+    //Route keuangan bank 
     Route::get('keuangan', 'KeuanganController@index')->name('keuangan'); //menampilkan data keuangan bank sampah dan saldo
     Route::post('tarik', 'PenarikanController@tarik')->name('admin_tarik'); //menarik saldo oleh admin dari keuangan
     
     //Route bendahara
-    Route::get('penarikan', 'PenarikanController@index')->name('penarikan'); //menampilkan data user
+    Route::get('penarikan', 'PenarikanController@index')->name('tarik_sis'); //menampilkan data user
     Route::get('saldo/{id}', 'BendaharaController@saldo')->name('saldo'); //menampilkan data saldo user dan request penarikanya berdasarkan id
     Route::post('penarikan/{id}', 'BendaharaController@tarik')->name('penarikan'); //mengkonfirmasi penarikan nasabah oleh bendahara
     
