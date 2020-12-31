eab8433d76df977bc5de602914aa8789b4d5adbe
hanifazzuhdi <hnfhanif52@gmail.com>
2020-12-31 13:58:16 +0700

    try fix jquery

--- public/js/script.js
+++ public/js/script.js
@@ -3,14 +3,6 @@
     headers: { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') }
     });
 
-    const proxyurl = "https://cors-anywhere.herokuapp.com/";
-    const url = "http://sammpah.herokuapp.com/"; // site that doesn’t send Access-Control-*
-    fetch(proxyurl + url) // https://cors-anywhere.herokuapp.com/https://example.com
-    .then(response => response.text())
-    .then(contents => console.log(contents))
-    .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
-
-
 // jquery + ajax update
     $(function () {
 
