37ff4cf203dbdf0ddb872a2e1185e261261fca98
icatpojan <irsyadfauzangg@gmail.com>
2021-01-02 20:38:34 +0700

    pull

--- app/Http/Controllers/Web/BendaharaController.php
+++ app/Http/Controllers/Web/BendaharaController.php
@@ -8,6 +8,8 @@ use App\Model\Keuangan;
 use App\Model\Penarikan;
 use App\Model\Penyetoran;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
 
 class BendaharaController extends Controller
 {
@@ -44,6 +46,47 @@ class BendaharaController extends Controller
             'debet'      => 0,
             'kredit'     => $penarikan->kredit,
             'saldo'      => Keuangan::latest()->first()->saldo - $penarikan->kredit
+        ]);
+    }
+    public function tarik_tunai(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'nama'      => 'required',
+            'nominal'   => 'required',
+            'user_id'   => 'required',
+        ]);
+        if ($validator->fails()) {
+            return response($validator->errors());
+        }
+        $data = Tabungan::where('user_id', request('user_id'))->latest()->first();
+
+        if ($data == null or request('nominal') > $data->saldo) {
+            return $this->sendResponse('Failed', 'Jual Sampah Dulu Biar Kaya', 'null', 404);
+        }
+
+        Tabungan::create([
+            'user_id'       => request('user_id'),
+            'keterangan'    => 'Penarikan Saldo',
+            'jenis_sampah'  => null,
+            'berat'         => null,
+            'debet'         => null,
+            'kredit'        => request('nominal'),
+            'saldo'         => $data->saldo - request('nominal')
+        ]);
+
+        Penarikan::create([
+            'user_id'       => request('user_id'),
+            'nama'          => request('nama'),
+            'rekening'      => null,
+            'kredit'        => request('nominal'),
+            'keterangan'    => 'Penarikan Saldo',
+            'status'        => 2,
+        ]);
+        Keuangan::create([
+            'keterangan' => 'Penarikan Uang Oleh Nasabah',
+            'debet'      => 0,
+            'kredit'     => request('nominal'),
+            'saldo'      => Keuangan::latest()->first()->saldo - request('nominal')
         ]);
     }
 }
