5c22de5ffda6f57aba04a2295dc23272ae4c7719
icatpojan <irsyadfauzangg@gmail.com>
2021-01-04 21:07:07 +0700

    pull

--- config/mail.php
+++ config/mail.php
@@ -36,7 +36,7 @@ return [
     'mailers' => [
         'smtp' => [
             'transport' => 'smtp',
-            'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+            'host' => env('MAIL_HOST', 'smtp.gmail.com'),
             'port' => env('MAIL_PORT', 587),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'username' => env('MAIL_USERNAME'),
@@ -84,8 +84,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'irsyadfauzangg@gmail.com'),
+        'name' => env('MAIL_FROM_NAME', 'icatpojan'),
     ],
 
     /*
