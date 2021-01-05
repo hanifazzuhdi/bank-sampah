a9198a0bd6e6fc793b8444bcab04fe2bc449b259
hanifazzuhdi <hnfhanif52@gmail.com>
2021-01-05 09:31:46 +0700

    update chart penghasilan

--- app/Http/Controllers/Web/HomeController.php
+++ app/Http/Controllers/Web/HomeController.php
@@ -9,6 +9,7 @@ use App\Model\Penyetoran;
 use App\Http\Controllers\Controller;
 use App\Model\Jenis;
 use App\Model\Sampah;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -19,33 +20,42 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $user = User::where('role_id', 1)->where('deleted_at', null)->count();
-
-        $keuangan = Keuangan::latest()->first('saldo');
         $month = date('m');
+
+        // Card
+        $user = User::where('role_id', 1)->where('deleted_at', null)->count();
+        $keuangan = Keuangan::latest()->first('saldo');
         $penjualan = Penjualan::whereMonth('created_at', $month)->sum('penghasilan');
         $transaksi = Penyetoran::whereMonth('created_at', $month)->count();;
 
+        // Pie grafik
         $jenis_sampah = Jenis::all();
-
         foreach ($jenis_sampah as $value) {
-            # code...
             $jenis[] = $value->jenis_sampah;
         }
 
         foreach ($jenis_sampah as $value) {
-            # code...
             $warna[] = $value->warna;
         }
 
         $sampahh = Sampah::all();
-
         foreach ($sampahh as $value) {
-            # code...
             $sampah[] = $value->berat;
         }
 
-        return view('pages.home', compact('user', 'keuangan', 'penjualan', 'transaksi', 'jenis', 'sampah', 'warna', 'jenis_sampah'));
+        // chart penghasilan
+        $penghasilann = DB::table("penjualans")
+            ->select(DB::raw("(SUM(penghasilan)) as penghasilan"))
+            ->groupBy(DB::raw("MONTH(created_at)"))
+            ->get();
+
+        foreach ($penghasilann as  $value) {
+            $penghasilan[] =  $value->penghasilan;
+        }
+
+        // dd($penghasilan);
+
+        return view('pages.home', compact('user', 'keuangan', 'penjualan', 'transaksi', 'jenis', 'sampah', 'warna', 'jenis_sampah', 'penghasilan'));
     }
 
     public function sampah()
--- public/template/js/demo/chart-area-demo.js
+++ public/template/js/demo/chart-area-demo.js
@@ -46,7 +46,7 @@ var myLineChart = new Chart(ctx, {
       pointHoverBorderColor: "rgba(78, 115, 223, 1)",
       pointHitRadius: 10,
       pointBorderWidth: 2,
-      data: [0, 10000, 5000, 15000, 10000, 20000, 15000, 25000, 20000, 30000, 25000, 40000],
+      data: [0, 10000, 5000, 15000, 10000, 20000, 15000, 25000, 20000, 30000, 25000, 1000000],
     }],
   },
   options: {
@@ -78,7 +78,7 @@ var myLineChart = new Chart(ctx, {
           padding: 10,
           // Include a dollar sign in the ticks
           callback: function(value, index, values) {
-            return '$' + number_format(value);
+            return 'IDR ' + number_format(value);
           }
         },
         gridLines: {
