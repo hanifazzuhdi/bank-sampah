83fa192e95be49472d7d4bbea2bc79438ca3e8a7
hanifazzuhdi <hnfhanif52@gmail.com>
2021-01-16 22:46:26 +0700

    fix bugs

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use NotificationChannels\Telegram\Telegram;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
