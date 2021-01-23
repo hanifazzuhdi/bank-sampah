1f5a5d1724cd5f5f5bd29a0abef1deab46566c2e
icatpojan <irsyadfauzangg@gmail.com>
2021-01-23 14:15:59 +0700

    oke

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -120,18 +120,15 @@
 
 <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SAMMPAH</title>
     <link rel="stylesheet" href="{{ asset('stylesheets.css') }}">
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-
 </head>
 
 <body>
     <header>
         <div class="container">
-            <div class="header-left">
-            
-            </div>
             <div class="header-right">
                 <a href="{{ route('login') }}" class="login">SAMMPAH</a>
             </div>
@@ -139,12 +136,13 @@
     </header>
     <div class="top-wrapper">
         <div class="container">
-            <h1>SAMM<span class="fa fa-twitter"></span>AH</h1>
+            <h1>SAMM<span class="fa  fa-twitter"></span>AH</h1>
             <h1>BUANG SAMPAH JADI UANG</h1>
             <p>Sammpah adalah platform online untuk mengelola limbah anda.</p>
             <p>Kami menawarkan sistem pengelolaan sampah yang mudah untuk kehidupan anda.</p>
             <div class="btn-wrapper">
-                <a href="https://play.google.com/"><img src="{{asset('google.svg')}}" alt="a" style="height: 75px"></a>
+                <a href="https://play.google.com/"><img src="{{ asset('google.svg') }}" alt="a"
+                        style="height: 75px"></a>
                 <p>lalu</p>
                 <a href="#" class="btn facebook"><span></span>Daftar dengan gmail</a>
                 <a href="#" class="btn twitter"><span></span>Daftar dengan email</a>
@@ -153,11 +151,13 @@
     </div>
     <footer>
         <div class="container">
-       <p> <a href="https://laracasts.com">Rais Azaria</a>
-        <a href="https://github.com/icatpojan">Irsyad Fauzan</a>
-        <a href="https://github.com/hanifazzuhdi">Hanif Az Zuhdi</a></p>
+            <p> <a href="https://laracasts.com">Rais Azaria</a>
+                <a href="https://github.com/icatpojan">Irsyad Fauzan</a>
+                <a href="https://github.com/hanifazzuhdi">Hanif Az Zuhdi</a>
+            </p>
         </div>
-      </footer>
+    </footer>
+    <script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
 </body>
 
 </html>
