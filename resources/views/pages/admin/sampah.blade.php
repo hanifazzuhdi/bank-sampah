2e32c3133869f46878432579d083003028636d87
icatpojan <irsyadfauzangg@gmail.com>
2021-01-24 07:55:01 +0700

    chart

--- resources/views/pages/admin/sampah.blade.php
+++ resources/views/pages/admin/sampah.blade.php
@@ -118,7 +118,7 @@
 			data: {
 				labels: {!! json_encode($jenis) !!},
 				datasets: [{
-					label: 'perbandingan harga',
+					label: 'per {{now()}}',
 					data: {!! json_encode($harga) !!},
 					backgroundColor: [
 					'rgba(255, 99, 132, 0.2)',
